ad1ad26d0a0588f341ef1d4a70981c6a898579fa
ps'killer <komliev1999@mail.ru>
2025-04-19 02:00:16 +0500

    1

---
+++ users/models.py
@@ -0,0 +1,36 @@
+﻿from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.CharField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Укажите телефон",
+    )
+
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        verbose_name="Аватар пользователя",
+        null=True,
+        blank=True,
+    )
+
+    country = models.CharField(
+        max_length=15, verbose_name="Страна", null=True, blank=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
