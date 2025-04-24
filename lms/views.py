687b6932b7bf531013d80e0602414b011545cf30
ps'killer <komliev1999@mail.ru>
2025-04-24 14:22:53 +0500

    1

--- lms/views.py
+++ lms/views.py
@@ -1,9 +1,44 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+    ListCreateAPIView,
+)
 
 from lms.models import Course
-from lms.serializers import CourseSerializer
+from lms.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(ModelViewSet):
-    queryser = Course.objects.all()
+    """ViewSet для моделей курсов."""
+
+    queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class LessonCreateApiView(CreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListApiView(ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveApiView(RetrieveAPIView):
+    queryset = Course.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateApiView(UpdateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyApiView(DestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = LessonSerializer
