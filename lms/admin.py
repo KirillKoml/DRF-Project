94569302232e7c992cd5bf2ef66019372697f1d7
ps'killer <komliev1999@mail.ru>
2025-05-14 20:33:56 +0500

    1

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from lms.models import Subscription
+
+
 # Register your models here.
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    """Админка для подписок"""
+    list_display = ('id', 'user', 'course')
